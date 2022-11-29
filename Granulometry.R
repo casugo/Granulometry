ae7922d02f948230351eabab91f0c84e74e9ecaa
casugo <catasues_21@hotmail.com>
2022-11-29 14:37:44 +0100

    Initial commit

--- Granulometry.R
+++ Granulometry.R
@@ -4,24 +4,16 @@ author: "Catalina Suescun"
 date: '2022-11-18'
 output: word_document
 ---
+  
 
 # Load Libraries
 library(tidyverse)
 library(here)
 library(kableExtra)
 library(ggpubr)
-
-## Ggplot Theme setting
-theme_set(theme_bw(base_size = 16, base_family = "Palatino")) 
-options(digits = 4)
-
-
-knitr::opts_chunk$set(echo = TRUE)
-# Load Libraries
-library(tidyverse)
-library(here)
-library(kableExtra)
-library(ggpubr)
+library(rmarkdown)
+library(usethis)
+usethis::create_github_token()
 
 
 
@@ -29,81 +21,20 @@ library(ggpubr)
 ## Loading the paths
 files <- 
   here::here("Data","Analysis") %>%
-
-
-## Creating the Nested dataframe ----
-Granulometry <- files %>%  map( ~ read.csv(.))
-Granulometry <- Granulometry %>%   set_names(files) %>%   enframe("Type of sifter", "Data")
-
-
-# Changing the names of the document
-nombres <- Data()[[1:4]]$...1 %>% na.omit()
-nombres <- c("+5mm", "1.5", "3mm", "5mm")
-
-
-
-## Organising the dataframe
-Granulometry <- 
-  Granulometry %>%  
-  separate(Data, 
-           sep = "/",
-           into = c(c(LETTERS[1:7]), c("Sample"))) %>% 
-  select(-c(LETTERS[1:9]), J, -c("K", "L"), "Sample", "Data")
-
-
-
-
-
-
-
-
-
-updating 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Data Loading ----
-## Loading the paths
-files <-
-  here("Granulometry", "data") %>% 
   dir( recursive=TRUE, full.names=TRUE, pattern="\\.csv$")
 
-
 ## Creating the Nested dataframe ----
 Granulometry <- files %>%  map( ~ read.csv(.))
-Granulometry <- Granulometry %>%   set_names(files) %>%   enframe("Sifter", "Data")
-
+Granulometry <- Granulometry %>%   set_names(files) %>% enframe("Size", "Datos")
 
 ## Organising the dataframe
 Granulometry <- 
   Granulometry %>%  
-  separate(Type, 
+  separate(Size,
            sep = "/",
-           into = c(c(LETTERS[1:12]), c("Sample"))
-  ) %>% 
-  select(-c(LETTERS[1:9]), J, -c("K", "L"), "Sample", "Data")
-
-# Changing the names
-colnames(Granulometry) <- c("Material", "Sample", "Data")
+           into = c(c(LETTERS[1:13]), c("Sample"))
+  )            %>% 
+  select(-c(LETTERS[1:13]), "Sample", "Datos")
 
 
 ## Adusting the Table
@@ -111,26 +42,65 @@ Granulometry <-
   Granulometry %>%  
   separate(Sample, 
            sep = "_",
-           into = c(c(LETTERS[1:7]))
+           into = c(c(LETTERS[1:2]))
   )
-
-# Granulometry$F %>% as.factor() %>% levels()
-
-# Histograph
+# Changing the names
+colnames(Granulometry) <- c("Material", "Sample", "Datos")
 
 # Unnesting the total dataframe for graphics
-Granulometry <- Granulometry %>% unnest(Data)
+Granulometry <- Granulometry %>% unnest(Datos)
 
-# Organizing Factors
-Granulometry$D <- factor(Granulometry$D, levels = c("700", "1400", "2100", "3000"))
-Granulometry$D %>% as.factor() %>% levels()
+Granulometry <- 
+  Granulometry %>%  
+  separate(X.Area.Mean.StdDev.Min.Max.Perim..Median, 
+           sep = ";",
+           into = c(c(LETTERS[1:8]))
+  ) %>% 
+  select(-c("A","C","D","E","F","G","H"))
 
-Granulometry %>%
-  ggplot( aes(x = Area, fill=F)) +
-  geom_density() +
-  #geom_histogram(binwidth=1 ) +
-  facet_grid( D ~ .) +
-  theme_minimal() 
+# Changing the names
+colnames(Granulometry) <- c( "Material", "Sample", "Area")
 
-ggsave(here("Figures","Granulometry", "Plot.jpg"), 
-       width=1500, height=800, dpi = 130, 
+
+#Transform Character columns in numbers
+Granulometry <- transform(Granulometry, Area = as.numeric(Area))
+
+#filtration of the data for size
+Size1.5 <- Granulometry %>%
+  filter(Sample=="1.5mm.csv")
+
+Size3 <- Granulometry %>%
+  filter(Sample=="3mm.csv")
+
+Size5 <- Granulometry %>%
+  filter(Sample=="5mm.csv")
+
+#Ploting size 1.5
+S1.5 = Size1.5$Area
+h = hist (S1.5,col =  "blue", xlab = "Area", ylab= "Number of particles",  main = "Granulometry 1.5mm")
+xfit = seq(min(S1.5), max(S1.5), length= 20)
+yfit = dnorm (xfit, mean = mean(S1.5),sd = sd(S1.5))
+yfit = yfit*diff(h$mids [1:2])* length (S1.5)
+lines (xfit , yfit, col ="red", lwd = 2)
+box()
+
+#Ploting size 3
+
+x = Size3$Area
+h = hist (x,col =  "blue", xlab = "Area", ylab= "Number of particles",  main = "Granulometry 3mm")
+xfit = seq(min(x), max(x), length= 20)
+yfit = dnorm (xfit, mean = mean(x),sd = sd(x))
+yfit = yfit*diff(h$mids [1:2])* length (x)
+lines (xfit , yfit, col ="red", lwd = 3)
+box()
+
+#Ploting size 5
+x = Size5$Area
+h = hist (x,col =  "blue", xlab = "Area", ylab= "Number of particles", main = "Granulometry 5mm")
+xfit = seq(min(x), max(x), length= 20)
+yfit = dnorm (xfit, mean = mean(x),sd = sd(x))
+yfit = yfit*diff(h$mids [1:2])* length (x)
+lines (xfit , yfit, col ="red", lwd = 3)
+box()
+
+
