22a7671d9ad275ac0dd512adcd165cc7cf775bc1
casugo <catasues_21@hotmail.com>
2023-01-09 00:15:46 +0100

    sqve imge

--- Granulometry.R
+++ Granulometry.R
@@ -87,3 +87,8 @@ Granulometry %>% filter(Granulometry != "+5mm" ) %>%
 
 ggsave( here("Figures/Granulometry.png") , width = 6, height = 8, dpi="print" )
 
+
+### Saving the Figure
+
+ggsave( here("Figures/Granulometry.png") , width = 6, height = 8, dpi="print" )
+
