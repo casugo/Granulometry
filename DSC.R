fea42b83b41133142539abf1dbae1b8c262976b6
fabbiocrux <fabbiocrux.ms@gmail.com>
2023-02-05 15:56:18 +0100

    retomando el proyecto de cata

---
+++ DSC.R
@@ -0,0 +1,13 @@
+# Data Analysis of Granulometry for Catalina
+## Date: 06/12/2022
+
+## Load Libraries -----
+library(tidyverse)
+library(here)
+library(kableExtra)
+library(ggpubr)
+library(rmarkdown)
+library(usethis)
+
+
+## Loading the data
